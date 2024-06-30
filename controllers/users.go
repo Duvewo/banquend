83bab6c8aa16f7b1e73b9f0a136c4542f232452e
Duvewo <duvewo@gmail.com>
2024-06-30 09:09:00 +0200

    new: controllers, routes; cardgen wip

--- controllers/users.go
+++ controllers/users.go
@@ -1,11 +1,7 @@
 package controllers
 
 import (
-	"context"
-	"net/http"
-
 	"github.com/Duvewo/banquend/handler"
-	"github.com/Duvewo/banquend/models"
 	"github.com/labstack/echo/v4"
 )
 
@@ -15,27 +11,8 @@ type UsersController struct {
 
 func (c UsersController) REGISTER(group *echo.Group) {
 	group.GET("/:query", c.ByQuery)
-	group.POST("/create", c.Create)
 }
 
 func (c UsersController) ByQuery(ctx echo.Context) error {
 	return nil
-}
-
-func (c UsersController) Create(ctx echo.Context) error {
-	user := models.UserModel{}
-	if err := c.Router.Binder.Bind(&user, ctx); err != nil {
-		c.Logger.Errorf("controllers/users: to bind %w", err)
-		return err
-
-	}
-
-	c.Logger.Infoln(user)
-
-	// user.Validate()
-
-	if err := c.Users.Create(context.Background(), user); err != nil {
-		return ctx.JSON(http.StatusBadGateway, echo.Map{"ok": false})
-	}
-	return ctx.JSON(http.StatusCreated, echo.Map{"ok": true})
 }
---
+++ internal/card/card.go
@@ -0,0 +1,11 @@
+package card
+
+import "github.com/Duvewo/banquend/models"
+
+func Generate(IssuerIndustry int8, IssuerIDNumber int32, AccountNumber int64) models.CardModel {
+
+}
+
+//func
+
+// 4; 1234 5; 123 1234 123; 1
